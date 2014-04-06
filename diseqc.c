61a39605ed035c78f8374ef276aeb7ce25e287d4
Bill Murphy <gc2majortom_at_mail_dot_com>
2014-04-06 18:24:19 -0400

    gotox function is more reliable with this 1 second delay

--- diseqc.c
+++ diseqc.c
@@ -125,6 +125,7 @@ void motor_gotox(int frontend_fd, int pmem)
     int sec = time(NULL);
     set_conio_terminal_mode();
     do {
+		sleep(1);
 		if ( kbhit() )
 	   		c = kbgetchar(); /* consume the character */
     } while( (char)c != 's' && sec+45 > time(NULL) );
